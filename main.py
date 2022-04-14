42c5cb13f38296074f2b5e1e376ac9b405d3ef4f
xxchillkroetexx <info@wolf-andreas.eu>
2022-04-14 15:25:09 +0200

    working get request and save

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import requests
 from bs4 import BeautifulSoup
 
 # Variablen
+# Link, der abgefragt werden soll
 link = "https://pes.ihk.de//Auswertung.cfm?Beruf={}"
+
 
 # Code
 # Berufenummern aus Datei in Liste speichern
@@ -13,24 +15,36 @@ idfile = open(
 berufids = idfile.readlines()
 idfile.close()
 
+
+def removeLastN(string, n):  # Letzen N Zeichen aus string entfernen
+    size = len(string)
+    string = string[:size - n]
+    return string
+
+
 for berufid in berufids:
+    # \n in berufid entfernen
+    berufid = removeLastN(berufid, 1)
+
     # Abfragelink  für jede ID erstellen
     abfrage = link.format(berufid)
-
+    print(abfrage)
     # Abfrage pro Bundesland oder IHK Standort
-    for zeile in ihkliste:
-        abfrage = abfrage + "&pm=" + \
-            str(zeile) + "&pm=" + str(zeile) + \
-            "&pm=" + str(zeile)  # neuer Abfragelink
+    # for zeile in ihkliste:
+    #    abfrage = abfrage + "&pm=" + \
+    #        str(zeile) + "&pm=" + str(zeile) + \
+    #        "&pm=" + str(zeile)  # neuer Abfragelink
+
     # Webabfrage erstellen
     r = requests.get(abfrage)
     # Antwort aus der Abfrage in Variable speichern
     response = r.text
 
-    # Webantwort in Datei für später speichern
-    savefile = open("H:/temp/python/{0}.txt".format(berufid), "w")
+    # Webantwort in Datei speichern
+    savepath = "H:/temp/python/{0}.html"
+
+    savepath = savepath.format(berufid)
+
+    savefile = open(savepath, "w")
     savefile.write(response)
     savefile.close()
-
-    # 5 Sekunden warten, bis nächste Abfrage ausgeführt wird
-    time.sleep(5)
